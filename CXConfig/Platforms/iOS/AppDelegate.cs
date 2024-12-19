936c3c4b38122ccc431c5f12822e98454e04f7bb
AdrianGumula <adrian.gumula@globallogic.com>
2024-12-19 22:14:36 +0100

    removed iOS target

--- CXConfig/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CXConfig;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
