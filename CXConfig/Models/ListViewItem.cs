cf5fbe55d3cd93923bbbe83a0c7aba049de7ed8e
Adrian Gumuła <70687019+adrgumula@users.noreply.github.com>
2024-12-22 13:26:18 +0100

    added description field to BottleItem

--- CXConfig/Models/ListViewItem.cs
+++ CXConfig/Models/ListViewItem.cs
@@ -1,13 +1,15 @@
 namespace CXConfig.View;
 public class BottleItem
 {
-    public BottleItem(string name, string folderFullPath, string configFullPath, string alternativeConfigFileFullpath){
+    public BottleItem(string name, string folderFullPath, string configFullPath, string description, string alternativeConfigFileFullpath ){
         Name = name;
         FolderFullPath = folderFullPath;
         ConfigFileFullPath = configFullPath;
         AlternativeConfigFileFullpath = alternativeConfigFileFullpath;
+        Description = description;
     }
     public string Name { get; set; }
+    public string Description {get;set;}
     public string FolderFullPath { get; set; }
     public string ConfigFileFullPath { get; set; } 
     public string AlternativeConfigFileFullpath { get; set; }
